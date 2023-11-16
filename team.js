2b1b8c890760068d5b6aaf5c9d65893ab8685954
Karim Felus <karim.fls@hotmail.com>
2023-11-16 17:06:37 +0100

    Añadir Objeto de Karim

--- team.js
+++ team.js
@@ -20,4 +20,18 @@ let variable=[{
     favoriteFilm: "Señor de los Anillos",
     favoriteBook: "Epic",
     petName: "Kenma",
+    let variable=[];
+
+let object = {
+    name: "Karim",
+    surname: "Felus",
+    age: 26,
+    city: "Barcelona",
+    hobby: "jugar",
+    favoriteFood: "Paella",
+    favoriteVideoGame: "Fifa",
+    favoriteFilm: "Tenet",
+    favoriteBook: "Harry Potter Saga",
+    petName: "Misha",
+};
 }]

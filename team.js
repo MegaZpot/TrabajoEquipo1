a60f0675cb30ec3ae039446d48491176b600956c
JavierRuizPorta <javierrp94@gmail.com>
2023-11-16 16:48:02 +0100

    On branch JavierRuiz  Changes to be committed: 	modified:   team.js

--- team.js
+++ team.js
@@ -1,1 +1,20 @@
 let variable=[];
+const nuevoObjeto = {
+    
+
+nombre: "Javier",
+apellido:"Ruiz",
+edad:30,
+ciudad: "Malaga",
+pasatiempo: "Programar",
+comidaFavorita: "Croquetas",
+videojuegoFavorito: "F1",
+peliculaFavorita: "Cualquiera de Batman",
+libroFavorito: "Comic de Eterno Hallowen de Batman",
+nombreMascota: "No tengo",
+}
+variable.push(nuevoObjeto);
+
+console.log(variable);
+
+

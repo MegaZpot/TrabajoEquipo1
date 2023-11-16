742d286ffd561f0e35fa9dbb317933cf03c07340
Vincent <megazpot@gmail.com>
2023-11-16 16:52:07 +0100

    Añado branch con informacion de Jose Vicente

--- team.js
+++ team.js
@@ -1,1 +1,13 @@
-let variable=[];
+let variable=[{
+    name : "Jose Vicente",
+    surname : "Jimenez Chamizo",
+    age : "27",
+    city : "Málaga",
+    hobby : "Videojuegos",
+    favoriteFood : "Pastel de carne",
+    favoriteVideoGame : "League of Legends",
+    favoriteFilm : "El Señor de los Anillos",
+    favoriteBook : "Berserk",
+    petName : "Kino"
+}];
+
